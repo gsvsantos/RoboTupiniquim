4d8dd5b43f933995f689a27f6094c02a84f2fe11
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-27 15:16:30 -0300

    feat: Adicionado métodos `IntVerify()` e `StringVerify()`.

--- RoboTupiniquim/Entities/Utils/Validators.cs
+++ RoboTupiniquim/Entities/Utils/Validators.cs
@@ -2,4 +2,57 @@
 
 public class Validators
 {
+    public static int IntVerify(string prompt, int minValue = 0, int maxValue = int.MaxValue)
+    {
+        do
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine()!;
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine($"'{input}' não é um número...");
+                continue;
+            }
+            input = input.Trim();
+            if (input.Contains(' ') || input.Contains('.') || input.Contains(','))
+            {
+                Console.WriteLine("O valor não pode conter espaço ' ', ponto (.), ou vírgula (,)!");
+                continue;
+            }
+            if (!int.TryParse(input, out int value))
+            {
+                Console.WriteLine("O valor digitado não é um número válido.");
+                continue;
+            }
+            if (value <= minValue || value > maxValue)
+            {
+                Console.WriteLine($"O valor deve estar entre 1 e {maxValue}.");
+                continue;
+            }
+            return value;
+        } while (true);
+    }
+    public static string StringVerify(string lenghtError, int minLenght)
+    {
+        do
+        {
+            string input = Console.ReadLine()!;
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine($"'{input}' não me parece correto...", ConsoleColor.Red);
+                continue;
+            }
+            if (!input.All(char.IsLetter))
+            {
+                Console.WriteLine($"Tem algo de errado com essa palavra... ({input})", ConsoleColor.Red);
+                continue;
+            }
+            if (input.Length < minLenght)
+            {
+                Console.WriteLine(lenghtError, ConsoleColor.Red);
+                continue;
+            }
+            return input;
+        } while (true);
+    }
 }
