a3743cbf14087d49c745e680511908521468684e
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-27 19:09:41 -0300

    refactor: Movido cabeçalho para `ViewWrite.Header()`.

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -1,10 +1,12 @@
-﻿    namespace RoboTupiniquim
+﻿using RoboTupiniquim.Entities.Utils;
+
+namespace RoboTupiniquim
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Projeto - Robô Tupiniquim");
+            ViewWrite.Header();
         }
     }
 }
