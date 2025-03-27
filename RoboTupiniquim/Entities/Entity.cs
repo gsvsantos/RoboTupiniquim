e80e23175c4d18a7556b7e1672b0736abece4565
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-27 20:44:00 -0300

    feat: Adicionado métodos de direção e movimento da `Entity`.

--- RoboTupiniquim/Entities/Entity.cs
+++ RoboTupiniquim/Entities/Entity.cs
@@ -1,8 +1,45 @@
-﻿namespace RoboTupiniquim.Entities;
+﻿using System.ComponentModel;
+
+namespace RoboTupiniquim.Entities;
 
 public class Entity
 {
-    public int PositionX {  get; set; }
-    public int PositionY { get; set; }
-    public char Direction { get; set; }
+    internal static int PositionX {  get; set; }
+    internal static int PositionY { get; set; }
+    internal static char Direction { get; set; }
+
+    public static void TurnLeft()
+    {
+        if (Direction == 'N')
+            Direction = 'O';
+        else if (Direction == 'O')
+            Direction = 'S';
+        else if (Direction == 'S')
+            Direction = 'L';
+        else if (Direction == 'L')
+            Direction = 'N';
+
+    }
+    public static void TurnRight()
+    {
+        if (Direction == 'N')
+            Direction = 'L';
+        else if (Direction == 'L')
+            Direction = 'S';
+        else if (Direction == 'S')
+            Direction = 'O';
+        else if (Direction == 'O')
+            Direction = 'N';
+    }
+    public static void MoveOn()
+    {
+        if (Direction == 'N')
+            PositionY += 1;
+        if (Direction == 'S')
+            PositionY -= 1;
+        if (Direction == 'L')
+            PositionX += 1;
+        if (Direction == 'O')
+            PositionX -= 1;
+    }
 }
