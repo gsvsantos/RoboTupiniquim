23cec5d80d4772ef63981593dd54894ab9f9b0ac
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-27 19:30:35 -0300

    feat: Adicionado métodos para imprimir mensagens de erros em `Validators`.

--- RoboTupiniquim/Entities/Utils/ViewWriteErrors.cs
+++ RoboTupiniquim/Entities/Utils/ViewWriteErrors.cs
@@ -1,5 +1,29 @@
 ﻿namespace RoboTupiniquim.Entities.Utils;
 
-internal class ViewWriteErrors
+public class ViewWriteErrors
 {
+    public static void InputIsNullOrEmpty(string input)
+    {
+        ViewColors.PaintWriteLine($"'{input}' não está certo...", ConsoleColor.Red);
+    }
+    public static void IntCannotHaveSpaceDotComma()
+    {
+        ViewColors.PaintWriteLine("O valor não pode conter espaço ' ', ponto (.), ou vírgula (,)!", ConsoleColor.Red);
+    }
+    public static void InvalidInt()
+    {
+        ViewColors.PaintWriteLine("O valor digitado não é um número válido.", ConsoleColor.Red);
+    }
+    public static void IntNeedBetweenMinAndMax(int maxValue)
+    {
+        ViewColors.PaintWriteLine($"O valor deve estar entre 1 e {maxValue}.", ConsoleColor.Red);
+    }
+    public static void InvalidString(string input)
+    {
+        ViewColors.PaintWriteLine($"Tem algo de errado com essa palavra... ({input})", ConsoleColor.Red);
+    }
+    public static void StringNeedBetweenMinAndMax(string lenghtError)
+    {
+        ViewColors.PaintWriteLine(lenghtError, ConsoleColor.Red);
+    }
 }
