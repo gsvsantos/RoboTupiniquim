e8b2b12b99445ba0c4df58aa231ab9582c5ab742
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-28 00:49:59 -0300

    feat: Adicionado método para validar se `Entity` está dentro do limite da `Area`.

--- RoboTupiniquim/Entities/Area.cs
+++ RoboTupiniquim/Entities/Area.cs
@@ -5,11 +5,6 @@ public class Area
     public static int MaxX { get; set; }
     public static int MaxY { get; set; }
 
-    public Area (int x, int y)
-    {
-        MaxX = x;
-        MaxY = y;
-    }
     public static bool RobotIsInside(int x, int y)
     {
         return x >= 0 && x <= MaxX && y >= 0 && y <= MaxY;
