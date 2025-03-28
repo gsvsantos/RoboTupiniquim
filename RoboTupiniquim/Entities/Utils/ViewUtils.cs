969c7be98d550311b2641766d655cd4935b93338
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-28 13:39:46 -0300

    feat: Adicionado `YesOrNo()` para que o usuário possa escolher acompanhar os passos dos Robôs.

--- RoboTupiniquim/Entities/Utils/ViewUtils.cs
+++ RoboTupiniquim/Entities/Utils/ViewUtils.cs
@@ -7,15 +7,32 @@ internal class ViewUtils
         string input = Validators.StringVerify("\nPor favor, digite o tamanho da área de pesquisa (Ex: 5 5): ", "Algo deu errado com o tamanho inserido da area de pesquisa!", 1, 3);
         return input.Split(' ');
     }
-    public static string[] GetRobotInitialData()
+    public static string[] GetRobotInitialData(int id)
     {
-        string input = Validators.StringVerify("Informe a posição inicial do Robô: ", "Houve um erro com esta posição informada!", 1, 5);
+        string input = Validators.StringVerify($"Informe a posição inicial do Robô #{id}: ", "Houve um erro com esta posição informada!", 1, 5);
         return input.ToUpper().Split(' ');
     }
     public static char[] GetCommandList()
     {
         string input = Validators.LetterVerify("Informe os comandos de movimentos: ", "Este comando está um pouco pequeno..", 1);
         return input.ToUpper().ToCharArray();
+    }
+    public static bool YesOrNo()
+    {
+        do
+        {
+            string option = Validators.LetterVerify("Gostaria de acompanhar os passos dos robôs? (S/N) ", "Há algo de errado com a opção escolhida..", 1, 1);
+            option = option.ToUpper();
+            if (option != "S" && option != "N")
+            {
+                ViewColors.PaintWriteLine("Opção inválida!", ConsoleColor.Red);
+                continue;
+            }
+            if (option == "S")
+                return true;
+            else
+                return false;
+        } while (true);
     }
     public static void PressEnter(string type)
     {
