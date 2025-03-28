7fa693569e57ba374d76bc198c1edf99994ffd7f
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-27 23:50:37 -0300

    feat: Adicionada herança de `Entity` em `Robot01`.

--- RoboTupiniquim/Entities/Robot01.cs
+++ RoboTupiniquim/Entities/Robot01.cs
@@ -1,5 +1,21 @@
 ﻿namespace RoboTupiniquim.Entities;
 
-internal class Robot01
+internal class Robot01 : Entity
 {
+    public override void TurnLeft()
+    {
+        base.TurnLeft();
+    }
+    public override void TurnRight()
+    {
+        base.TurnRight();
+    }
+    public override void MoveOn()
+    {
+        base.MoveOn();
+    }
+    public override void GetData()
+    {
+        base.GetData();
+    }
 }
