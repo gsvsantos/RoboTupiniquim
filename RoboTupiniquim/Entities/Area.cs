e53a8d3f79a20bd0c82b363925f5614ef136fa26
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-28 18:58:00 -0300

    refactor: Adicionado variáveis e métodos relacionados a `Area`.

--- RoboTupiniquim/Entities/Area.cs
+++ RoboTupiniquim/Entities/Area.cs
@@ -1,5 +1,19 @@
-﻿namespace RoboTupiniquim.Entities;
+﻿using RoboTupiniquim.Entities.Utils;
+
+namespace RoboTupiniquim.Entities;
 
 internal class Area
 {
+    public static int MaxX, MaxY;
+    public static void AreaCreate()
+    {
+        string[] values = Validators.AreaLimitVerify();
+        MaxX = Convert.ToInt32(values[0]);
+        MaxY = Convert.ToInt32(values[1]);
+    }
+    public static bool RobotIsInside(int positionX, int positionY)
+    {
+        return positionX >= 0 && positionX <= MaxX && positionY >= 0 && positionY <= MaxY;
+    }
+
 }
