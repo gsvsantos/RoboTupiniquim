15e514d28590e955b6094eb3b51b17fc01b4b79b
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-28 13:45:59 -0300

    feat: Adicionado método estático `id` e feita herança de `Entity` em `Robot`.

--- RoboTupiniquim/Entities/Robot.cs
+++ RoboTupiniquim/Entities/Robot.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace RoboTupiniquim.Entities;
 
-namespace RoboTupiniquim.Entities
+internal class Robot : Entity
 {
-    internal class Robot
+    public static int id = 0;
+    public Robot() { }
+    public override void TurnLeft(bool watchRobotSteps)
     {
+        base.TurnLeft(watchRobotSteps);
+    }
+    public override void TurnRight(bool watchRobotSteps)
+    {
+        base.TurnRight(watchRobotSteps);
+    }
+    public override void MoveOn(bool watchRobotSteps)
+    {
+        base.MoveOn(watchRobotSteps);
+    }
+    public override void GetData(int id)
+    {
+        base.GetData(id);
     }
 }
