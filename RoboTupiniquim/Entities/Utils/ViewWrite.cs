22d4f1c5d7f404b2ec15a8416a4a4322a296407b
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-28 04:42:35 -0300

    feat: Adicionado mais métodos de impressões de mensagens gerais.

--- RoboTupiniquim/Entities/Utils/ViewWrite.cs
+++ RoboTupiniquim/Entities/Utils/ViewWrite.cs
@@ -11,6 +11,22 @@ public class ViewWrite
     }
     public static void PrintArea()
     {
-        Console.WriteLine($"Área definida com limites de: ({Area.MaxX}, {Area.MaxY})");
+        ViewColors.PaintWriteLine($"Área definida com limites de: {Area.MaxX},{Area.MaxY}\n");
+    }
+    public static void LookToDirection(char oldDirection, char newDirection)
+    {
+        ViewColors.PaintWriteLine($"Virou a direção de {oldDirection} para {newDirection}.\n");
+    }
+    public static void InitialPosition(int positionX, int positionY, char direction)
+    {
+        ViewColors.PaintWriteLine($"\nPosição inical do Robô: {positionX},{positionY},{direction}\n");
+    }
+    public static void MovingOnPositions(int positionX, int positionY, char direction)
+    {
+        ViewColors.PaintWriteLine($"Moveu para: {positionX},{positionY},{direction}\n");
+    }
+    public static void LastPosition(int id, int positionX, int positionY, char direction)
+    {
+        ViewColors.PaintWriteLine($"Posição final do Robô #{id}: {positionX},{positionY},{direction}\n");
     }
 }
