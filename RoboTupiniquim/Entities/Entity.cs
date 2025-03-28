c4958a111c1615f9025c6578e9cb19a14afc5e90
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-28 02:36:14 -0300

    feat: Adicionado método `GetCommands()` para lidar com os comandos passados a `Entity` pelo usuário.

--- RoboTupiniquim/Entities/Entity.cs
+++ RoboTupiniquim/Entities/Entity.cs
@@ -4,7 +4,7 @@ namespace RoboTupiniquim.Entities;
 
 public class Entity
 {
-    protected static int PositionX {  get; set; }
+    protected static int PositionX { get; set; }
     protected static int PositionY { get; set; }
     protected static char Direction { get; set; }
 
@@ -35,20 +35,20 @@ public class Entity
     {
         if (Direction == 'N')
             PositionY += 1;
-        if (Direction == 'S')
+        else if (Direction == 'S')
             PositionY -= 1;
-        if (Direction == 'L')
+        else if (Direction == 'L')
             PositionX += 1;
-        if (Direction == 'O')
-            PositionX -= 1; 
-        
+        else if (Direction == 'O')
+            PositionX -= 1;
+
         if (Area.RobotIsInside(PositionX, PositionX))
         {
             Console.WriteLine($"Movido para ({PositionX}, {PositionY})");
         }
         else
         {
-            Console.WriteLine("Movimento inválido! Fora dos limites da área.");
+            Console.WriteLine("Movimento inválido! Fora dos limites da área de pesquisa.");
         }
     }
     public virtual void GetData()
@@ -57,5 +57,18 @@ public class Entity
         PositionX = Convert.ToInt32(data[0]);
         PositionY = Convert.ToInt32(data[1]);
         Direction = Convert.ToChar(data[2]);
+    }
+    public virtual void GetCommands()
+    {
+        char[] commands = Validators.RobotCommandsVerify();
+        foreach (char c in commands)
+        {
+            if (c == 'D')
+                TurnRight();
+            else if (c == 'E')
+                TurnLeft();
+            else if (c == 'M')
+                MoveOn();
+        }
     }
 }
