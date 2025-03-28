16471e09d9c68f1d81ab158d04923e95426b92f0
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-28 04:49:41 -0300

    feat: Iniciada primeira versão funcional do programa, utilizando as classes com seus métodos.

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -1,12 +1,29 @@
-﻿using RoboTupiniquim.Entities.Utils;
+﻿using RoboTupiniquim.Entities;
+using RoboTupiniquim.Entities.Utils;
 
 namespace RoboTupiniquim
 {
     internal class Program
     {
+        static Robot01 firstRobot = new Robot01();
+        static Robot02 secondRobot = new Robot02();
+
         static void Main(string[] args)
         {
             ViewWrite.Header();
+            Area.AreaCreate();
+            ViewWrite.PrintArea();
+
+            firstRobot.GetData();
+            firstRobot.GetCommands();
+
+            secondRobot.GetData();
+            secondRobot.GetCommands();
+
+            ViewWrite.LastPosition(firstRobot.Id, firstRobot.PositionX, firstRobot.PositionY, firstRobot.Direction);
+            ViewWrite.LastPosition(secondRobot.Id, secondRobot.PositionX, secondRobot.PositionY, secondRobot.Direction);
+
+            ViewUtils.PressEnter("USAR-NOVAMENTE");
         }
     }
 }
