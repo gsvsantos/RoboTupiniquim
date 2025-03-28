67fba167f6a262fe7616b4b387ab5a7fd163e51a
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-28 15:08:40 -0300

    refactor: Refatoração de `Entity` para `Robot`, e exclusão de `Robot01` e `Robot02`.

--- RoboTupiniquim/Entities/Robot.cs
+++ RoboTupiniquim/Entities/Robot.cs
@@ -1,23 +1,96 @@
-﻿namespace RoboTupiniquim.Entities;
+﻿using RoboTupiniquim.Entities.Utils;
+
+namespace RoboTupiniquim.Entities;
 
 internal class Robot : Entity
 {
     public static int id = 0;
-    public Robot() { }
-    public override void TurnLeft(bool watchRobotSteps)
+
+    public Robot()
     {
-        base.TurnLeft(watchRobotSteps);
+        Id = ++id;
     }
-    public override void TurnRight(bool watchRobotSteps)
+    public void TurnLeft(bool watchRobotSteps)
     {
-        base.TurnRight(watchRobotSteps);
+        char oldDirection = Direction;
+
+        if (Direction == 'N')
+            Direction = 'O';
+        else if (Direction == 'O')
+            Direction = 'S';
+        else if (Direction == 'S')
+            Direction = 'L';
+        else if (Direction == 'L')
+            Direction = 'N';
+
+        if (watchRobotSteps == true)
+            ViewWrite.LookToDirection(oldDirection, Direction);
     }
-    public override void MoveOn(bool watchRobotSteps)
+    public void TurnRight(bool watchRobotSteps)
     {
-        base.MoveOn(watchRobotSteps);
+        char oldDirection = Direction;
+
+        if (Direction == 'N')
+            Direction = 'L';
+        else if (Direction == 'L')
+            Direction = 'S';
+        else if (Direction == 'S')
+            Direction = 'O';
+        else if (Direction == 'O')
+            Direction = 'N';
+
+        if (watchRobotSteps == true)
+            ViewWrite.LookToDirection(oldDirection, Direction);
     }
-    public override void GetData(int id)
+    public void MoveOn(bool watchRobotSteps)
     {
-        base.GetData(id);
+        if (Direction == 'N')
+            PositionY += 1;
+        else if (Direction == 'S')
+            PositionY -= 1;
+        else if (Direction == 'L')
+            PositionX += 1;
+        else if (Direction == 'O')
+            PositionX -= 1;
+
+        if (watchRobotSteps == true)
+        {
+            ViewWrite.MovingOnPositions(PositionX, PositionY, Direction);
+        }
+    }
+    public void GetData()
+    {
+        string[] data = Validators.RobotDataVerify(Id);
+        PositionX = Convert.ToInt32(data[0]);
+        PositionY = Convert.ToInt32(data[1]);
+        Direction = Convert.ToChar(data[2]);
+    }
+    public void GetCommands()
+    {
+        do
+        {
+            char[] commands = Validators.RobotCommandsVerify();
+            ViewWrite.InitialPosition(PositionX, PositionY, Direction);
+            bool watchRobotSteps = ViewUtils.YesOrNo();
+            Console.WriteLine();
+
+            foreach (char c in commands)
+            {
+                if (c == 'D')
+                    TurnRight(watchRobotSteps);
+                else if (c == 'E')
+                    TurnLeft(watchRobotSteps);
+                else if (c == 'M')
+                    MoveOn(watchRobotSteps);
+            }
+
+            if (!Area.RobotIsInside(PositionX, PositionX))
+            {
+                ViewWriteErrors.InvalidLastPosition();
+                continue;
+            }
+            else
+                break;
+        } while (true);
     }
 }
