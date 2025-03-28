8588d96243b96679e82bd61dabffba3a43eb5d69
gsvsantos <gustavocnsantos02@gmail.com>
2025-03-28 15:14:03 -0300

    refactor: Modificada ordem dos métodos e corrigida escritas dos textos.

--- RoboTupiniquim/Entities/Utils/ViewWrite.cs
+++ RoboTupiniquim/Entities/Utils/ViewWrite.cs
@@ -7,23 +7,23 @@ public class ViewWrite
         Console.Clear();
         ViewColors.PaintWriteLine("/=-=-=-=-=-=-=-=-=-=-=-=\\");
         ViewColors.PaintWriteLine("     Robô Tupiniquim", ConsoleColor.Cyan);
-        ViewColors.PaintWriteLine("\\=-=-=-=-=-=-=-=-=-=-=-=/");
+        ViewColors.PaintWriteLine("\\=-=-=-=-=-=-=-=-=-=-=-=/\n");
     }
     public static void PrintArea()
     {
-        ViewColors.PaintWriteLine($"Área definida com limites de: {Area.MaxX},{Area.MaxY}\n");
-    }
-    public static void LookToDirection(char oldDirection, char newDirection)
-    {
-        ViewColors.PaintWriteLine($"Virou a direção de {oldDirection} para {newDirection}.\n");
+        ViewColors.PaintWriteLine($"Área definida com limites de: {Area.MaxX},{Area.MaxY}");
     }
     public static void InitialPosition(int positionX, int positionY, char direction)
     {
-        ViewColors.PaintWriteLine($"\nPosição inical do Robô: {positionX},{positionY},{direction}\n");
+        ViewColors.PaintWriteLine($"\nPosição inicial do Robô: {positionX},{positionY},{direction}\n", ConsoleColor.Green);
+    }
+    public static void LookToDirection(char oldDirection, char newDirection)
+    {
+        ViewColors.PaintWriteLine($"Virou a direção de {oldDirection} para {newDirection}.", ConsoleColor.Magenta);
     }
     public static void MovingOnPositions(int positionX, int positionY, char direction)
     {
-        ViewColors.PaintWriteLine($"Moveu para: {positionX},{positionY},{direction}\n");
+        ViewColors.PaintWriteLine($"Moveu para frente: {positionX},{positionY},{direction}", ConsoleColor.Blue);
     }
     public static void LastPosition(int id, int positionX, int positionY, char direction)
     {
