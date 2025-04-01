40dcfb69b82888216b686b31acc38c8c62bfdf66
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-01 02:51:05 -0300

    feat: Criada `Entity` para conter as propriedades básicas do `Robot`.

---
+++ RoboTupiniquim/Entities/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace RoboTupiniquim.Entities;
+
+public class Entity
+{
+    public int PositionX { get; set; }
+    public int PositionY { get; set; }
+    public char Direction { get; set; }
+    public int Id;
+}
